42215fc834c6c03b7a28f37782d573a63d57a943
Eric Greene <eric@t4d.io>
2020-11-02 18:12:35 -0500

    add exercise 4

---
+++ demo-app/exercises/exercise-4.md
@@ -0,0 +1,13 @@
+# Exercise 4
+
+1. Move the array of cars to `index.tsx`.
+
+2. Pass the array of cars as props into `CarTool` component.
+
+3. Utilize the array of cars passed in as props to dynamically populate the table of cars.
+
+4. Create a new component named `CarTable`. Move the table of cars to the `CarTable` component.
+
+5. Utilize the `CarTable` component in `CarTool`.
+
+6. Ensure it works!
--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { Color } from "./models/colors";
+
 import { ColorTool } from "./components/ColorTool";
 import { CarTool } from "./components/CarTool";
 
+const colorList: Color[] = [
+  { id: 1, name: "red", hexcode: "FF0000" },
+  { id: 2, name: "blue", hexcode: "0000FF" },
+  { id: 3, name: "white", hexcode: "FFFFFF" },
+  { id: 4, name: "green", hexcode: "00FF00" },
+  { id: 5, name: "brown", hexcode: "A52A2A" },
+  { id: 6, name: "aqua", hexcode: "00FFFF" },
+];
+
 render(
-  // React.createElement(ColorTool),
   <>
-    <ColorTool />
+    {/* React.createElement(ColorTool, { colors: colorList, headerText: 'Color Tool }) */}
+    <ColorTool colors={colorList} headerText="Color Tool" />
     <CarTool />
   </>,
   // "#root" - CSS selector - used to find an element with an id of 'root'
