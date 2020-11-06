6dc87ea7167693ef970e7e9707815d6da2a95fe1
Eric Greene <eric@t4d.io>
2020-11-06 13:45:42 -0500

    add exercise 18

---
+++ demo-app/exercises/exercise-18.md
@@ -0,0 +1,9 @@
+# Exercise 18
+
+1. Implement sorting on car table. When you click the column header, sort the array of cars on that column. Do not implement sorting for the Actions column.
+
+2. When you click the column header the first time, sort ascending. When click it the second time, sort descending.
+
+3. Display an arrow on the column (^ and v) to indicate the current sort order.
+
+4. Ensure it works.
--- demo-app/src/containers/CarToolContainer.tsx
+++ demo-app/src/containers/CarToolContainer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { bindActionCreators } from "redux";
 import { useSelector, useDispatch } from "react-redux";
 
+import { Car } from "../models/cars";
 import {
   createAppendCarAction,
   createReplaceCarAction,
@@ -12,10 +13,27 @@ import {
 import { CarTool } from "../components/CarTool";
 import { CarToolState } from "../models/carStore";
 
+// type CarKeys = "make" | "model" | "year" | "color" | "price";
+type CarKeys = keyof Car;
+
 export function CarToolContainer() {
   const stateProps = useSelector((state: CarToolState) => {
+    let sortCol: CarKeys;
+
+    sortCol = "make";
+
+    const sortedCars = [...state.cars].sort((a, b) => {
+      if (a[sortCol] < b[sortCol]) {
+        return -1;
+      } else if (a[sortCol] > b[sortCol]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
     return {
-      cars: state.cars,
+      cars: sortedCars,
       editCarId: state.editCarId,
     };
   });
