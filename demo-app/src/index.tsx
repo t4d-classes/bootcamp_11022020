30f2a1d107e1c4e99af1ec9bb3d9b4045119dc08
Eric Greene <eric@t4d.io>
2020-11-09 13:52:00 -0500

    add exercise for grouping set timeout

--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
+// import React from "react";
+// import { render } from "react-dom";
+// import { Provider } from "react-redux";
 
-import { calcToolStore } from "./stores/calcToolStore";
-import { CalcToolContainer } from "./containers/CalcToolContainer";
+// import { calcToolStore } from "./stores/calcToolStore";
+// import { CalcToolContainer } from "./containers/CalcToolContainer";
 
-render(
-  <Provider store={calcToolStore}>
-    <CalcToolContainer />
-  </Provider>,
-  document.querySelector("#root")
-);
+// render(
+//   <Provider store={calcToolStore}>
+//     <CalcToolContainer />
+//   </Provider>,
+//   document.querySelector("#root")
+// );
+
+export const doit = () => {};
